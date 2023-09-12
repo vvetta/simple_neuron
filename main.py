08b59a1a7fe41d85ca115b5f9d55a9c298be9295
vvetta <drpsih2004@gmail.com>
2023-09-12 18:52:53 +0300

    some fix

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ class Neuron:
 
 def main():
     
-    km = 100
-    miles = 62.1371
+    km = 1
+    miles = 0.621371
 
     nr = Neuron()
     i = 0
@@ -35,7 +35,9 @@ def main():
         print(f'Шаг - {i}, Вес - {nr.weight}, Последняя ошибка - {nr.lastError}')
         i += 1
             
-    print(nr.processInputData(km))
+    print(nr.processInputData(1))
+    print(nr.processInputData(1000))
+    print(nr.processInputData(543))
       
 
 if __name__ == "__main__":
